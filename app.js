fc99db43c8e45352e559e73e47f50ff186ea2d6c
HoussamMrabet <dodslim100@gmail.com>
2020-12-19 13:14:00 +0100

    cursor animation

--- app.js
+++ app.js
@@ -50,6 +50,34 @@ function animateSlides() {
     });
 }
 
+let mouse = document.querySelector('.cursor');
+let mouseTxt = mouse.querySelector('span');
 
+function cursor(e) {
+    mouse.style.top = e.pageY + 'px';
+    mouse.style.left = e.pageX + 'px';
+}
+
+function activeCursor(e) {
+    const item = e.target;
+    if (item.id === 'logo' || item.classList.contains('burger')) {
+        mouse.classList.add('nav-active');
+    } else {
+        mouse.classList.remove('nav-active');
+    }
+
+    if (item.classList.contains('explore')) {
+        mouse.classList.add('explore-active');
+        gsap.to('.title-swipe', 1, { y: '0%' });
+        mouseTxt.innerText = 'Tap';
+    } else {
+        mouse.classList.remove('explore-active');
+        gsap.to('.title-swipe', 1, { y: '100%' });
+        mouseTxt.innerText = '';
+    }
+}
+
+window.addEventListener('mousemove', cursor);
+window.addEventListener('mouseover', activeCursor);
 
 animateSlides();
