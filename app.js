59da666d26bdfe5c79e1527a819ac6d76b62402e
HoussamMrabet <dodslim100@gmail.com>
2020-12-19 13:43:47 +0100

    show and hide the menu

--- app.js
+++ app.js
@@ -1,83 +1,108 @@
-let controller;
-let slideScene;
-let pageScene;
+//Magic Scroller
 
-function animateSlides() {
-    //Init Controller
-    controller = new ScrollMagic.Controller();
+    let controller;
+    let slideScene;
+    let pageScene;
+
+    function animateSlides() {
+        //Init Controller
+        controller = new ScrollMagic.Controller();
 
 
-    const sliders = document.querySelectorAll('.slide');
-    const nav = document.querySelector('.nav-header');
+        const sliders = document.querySelectorAll('.slide');
+        const nav = document.querySelector('.nav-header');
 
-    sliders.forEach((slide, index, slides) => {
-        const revealImg = slide.querySelector('.reveal-img');
-        const img = slide.querySelector('img');
-        const revealText = slide.querySelector('.reveal-text');
+        sliders.forEach((slide, index, slides) => {
+            const revealImg = slide.querySelector('.reveal-img');
+            const img = slide.querySelector('img');
+            const revealText = slide.querySelector('.reveal-text');
 
-        //GSAP
-        const slideTl = gsap.timeline({ default: { duration: 1, ease: 'power2.inOut' } });
-        slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
-        slideTl.fromTo(img, { scale: '2' }, { scale: '1' }, '-=1');
-        slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.7');
-        slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5');
+            //GSAP
+            const slideTl = gsap.timeline({ default: { duration: 1, ease: 'power2.inOut' } });
+            slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
+            slideTl.fromTo(img, { scale: '2' }, { scale: '1' }, '-=1');
+            slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.7');
+            slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5');
 
-        //Create Scene
-        slideScene = new ScrollMagic.Scene({
-            triggerElement: slide,
-            triggerHook: 0.25,
-            reverse: false
-        })
-            .setTween(slideTl)
-            .addTo(controller);
-        
-        //New Animation
-        const pageTl = gsap.timeline();
-        let nextSlide = (slide.length - 1 === index) ? 'end' : slides[index + 1];
-        pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
-        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
-        pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5');
+            //Create Scene
+            slideScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                triggerHook: 0.25,
+                reverse: false
+            })
+                .setTween(slideTl)
+                .addTo(controller);
+            
+            //New Animation
+            const pageTl = gsap.timeline();
+            let nextSlide = (slide.length - 1 === index) ? 'end' : slides[index + 1];
+            pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
+            pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+            pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5');
 
-        //Create new scene
-        pageScene = new ScrollMagic.Scene({
-            triggerElement: slide,
-            duration: '100%',
-            triggerHook: 0
-        })
-            .setPin(slide, {pushFollowers: false})
-            .setTween(pageTl)
-            .addTo(controller);
-    });
-}
-
-let mouse = document.querySelector('.cursor');
-let mouseTxt = mouse.querySelector('span');
-
-function cursor(e) {
-    mouse.style.top = e.pageY + 'px';
-    mouse.style.left = e.pageX + 'px';
-}
-
-function activeCursor(e) {
-    const item = e.target;
-    if (item.id === 'logo' || item.classList.contains('burger')) {
-        mouse.classList.add('nav-active');
-    } else {
-        mouse.classList.remove('nav-active');
+            //Create new scene
+            pageScene = new ScrollMagic.Scene({
+                triggerElement: slide,
+                duration: '100%',
+                triggerHook: 0
+            })
+                .setPin(slide, {pushFollowers: false})
+                .setTween(pageTl)
+                .addTo(controller);
+        });
     }
 
-    if (item.classList.contains('explore')) {
-        mouse.classList.add('explore-active');
-        gsap.to('.title-swipe', 1, { y: '0%' });
-        mouseTxt.innerText = 'Tap';
-    } else {
-        mouse.classList.remove('explore-active');
-        gsap.to('.title-swipe', 1, { y: '100%' });
-        mouseTxt.innerText = '';
-    }
-}
+//Variables
+    const mouse = document.querySelector('.cursor');
+    const mouseTxt = mouse.querySelector('span');
+    const burger = document.querySelector('.burger');
 
-window.addEventListener('mousemove', cursor);
-window.addEventListener('mouseover', activeCursor);
+//functions
+    function cursor(e) {
+        mouse.style.top = e.pageY + 'px';
+        mouse.style.left = e.pageX + 'px';
+    }
+
+    function activeCursor(e) {
+        const item = e.target;
+        if (item.id === 'logo' || item.classList.contains('burger')) {
+            mouse.classList.add('nav-active');
+        } else {
+            mouse.classList.remove('nav-active');
+        }
+
+        if (item.classList.contains('explore')) {
+            mouse.classList.add('explore-active');
+            gsap.to('.title-swipe', 1, { y: '0%' });
+            mouseTxt.innerText = 'Tap';
+        } else {
+            mouse.classList.remove('explore-active');
+            gsap.to('.title-swipe', 1, { y: '100%' });
+            mouseTxt.innerText = '';
+        }
+    }
+    
+    function navToggle(e) {
+        if (!e.target.classList.contains('active')) {
+            e.target.classList.add('active');
+            gsap.to('.line1', 0.5, { rotate: '45' , y: 5, background: 'black'});
+            gsap.to('.line2', 0.5, { rotate: '-45', y: -5, background: 'black'});
+            gsap.to('#logo', 1, { color: 'black'});
+            gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+            document.body.classList.add('.hide');
+        } else {
+            e.target.classList.remove('active');
+            gsap.to('.line1', 0.5, { rotate: '0' , y: 0, background: 'white'});
+            gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white'});
+            gsap.to('#logo', 1, { color: 'white'});
+            gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+            document.body.classList.remove('.hide');
+        }
+    }
+
+//Event Listeners
+    burger.addEventListener('click', navToggle)
+    window.addEventListener('mousemove', cursor);
+    window.addEventListener('mouseover', activeCursor);
 
 animateSlides();
