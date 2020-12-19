27fb41c99dc33895c7b059397ac59ca7c07db190
HoussamMrabet <dodslim100@gmail.com>
2020-12-19 12:43:31 +0100

    overload scrolling animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     //Init Controller
@@ -9,7 +10,7 @@ function animateSlides() {
     const sliders = document.querySelectorAll('.slide');
     const nav = document.querySelector('.nav-header');
 
-    sliders.forEach(slide => {
+    sliders.forEach((slide, index, slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
@@ -20,7 +21,35 @@ function animateSlides() {
         slideTl.fromTo(img, { scale: '2' }, { scale: '1' }, '-=1');
         slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=0.7');
         slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=0.5');
+
+        //Create Scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse: false
+        })
+            .setTween(slideTl)
+            .addTo(controller);
+        
+        //New Animation
+        const pageTl = gsap.timeline();
+        let nextSlide = (slide.length - 1 === index) ? 'end' : slides[index + 1];
+        pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
+        pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+        pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' }, '-=0.5');
+
+        //Create new scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+            .setPin(slide, {pushFollowers: false})
+            .setTween(pageTl)
+            .addTo(controller);
     });
 }
+
+
 
 animateSlides();
